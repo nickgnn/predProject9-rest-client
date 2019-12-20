5b53987d64f2224857e6024b668375dd6d797144
nick <gnn@mm.st>
2019-12-20 18:05:22 +0300

    add buttons

--- src/main/java/ru/javamentor/predProject9_rest_client/controllers/AdminController.java
+++ src/main/java/ru/javamentor/predProject9_rest_client/controllers/AdminController.java
@@ -1,5 +1,6 @@
 package ru.javamentor.predProject9_rest_client.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -7,11 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import ru.javamentor.predProject9_rest_client.domain.User;
+import ru.javamentor.predProject9_rest_client.service.UserService;
 
 @Controller
 public class AdminController {
+    @Autowired
+    UserService userService;
+
     @PostMapping("/adminRest")
     public ResponseEntity<User> getReq(@RequestBody User user) {
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        User oneUser = userService.getOneUser(user.getId());
+
+        return new ResponseEntity<>(oneUser, HttpStatus.OK);
     }
+
+//    @GetMapping
+//    public ResponseEntity<User> getUser(@RequestBody Long id) {
+//        User oneUser = userService.getOneUser(id);
+//
+//        return new ResponseEntity<>(oneUser, HttpStatus.OK);
+//    }
 }
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function getUsername() {
     //function which send ajax request to the server
     $.ajax({
         url : '/adminRest',
@@ -6,14 +6,47 @@ function myFunction() {
         type : "post",
         contentType : "application/json",
         data : JSON.stringify({
-            username : 'Nick',
-            id : 1,
-            age : 31
+            id : '2'
         }),
 
         success : function(data) {
             console.log(data);
-            $("#text1").text(data.age);
+            $("#text1").text(data.password).text(data.username).text(data.age);
+        }
+    });
+}
+
+function getAge() {
+    //function which send ajax request to the server
+    $.ajax({
+        url : '/adminRest',
+        datatype : 'json',
+        type : "post",
+        contentType : "application/json",
+        data : JSON.stringify({
+            age : '31'
+        }),
+
+        success : function(data) {
+            console.log(data);
+            $("#text2").text(data.age);
+        }
+    });
+}
+
+function getUser() {
+    //function which send ajax request to the server
+    $.ajax({
+        url : '/adminRest',
+        datatype : 'json',
+        type : "post",
+        contentType : "application/json",
+        data : JSON.stringify({
+        }),
+
+        success : function(data) {
+            console.log(data);
+            $("#text3").text(data);
         }
     });
 }
