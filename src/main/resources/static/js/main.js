4e68ddc125e96d4a9ade9d84f0dd2d24349e8795
nick <gnn@mm.st>
2019-12-24 19:19:07 +0300

    remove functions

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,47 +9,11 @@ function getUsername() {
             id : '2'
         }),
 
-
-
         success : function(data) {
             console.log(data);
-            $("#text1").text(data.username);
-        }
-    });
-}
-
-function getAge() {
-    //function which send ajax request to the server
-    $.ajax({
-        url : '/users',
-        datatype : 'json',
-        type : "post",
-        contentType : "application/json",
-        data : JSON.stringify({
-            id : '2'
-        }),
-
-        success : function(data) {
-            console.log(data);
-            $("#text2").text(data.age);
-        }
-    });
-}
-
-function getUser() {
-    //function which send ajax request to the server
-    $.ajax({
-        url : '/users',
-        datatype : 'json',
-        type : "post",
-        contentType : "application/json",
-        data : JSON.stringify({
-            id : '2'
-        }),
-
-        success : function(data) {
-            console.log(data);
-            $("#text3").text(data.password);
+            $("#text1").text(data.username + '\n');
+            $("#text2").text(data.age + '\n');
+            $("#text3").text(data.password)
         }
     });
 }
